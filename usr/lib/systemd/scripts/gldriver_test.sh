f5fc8ead3d543ae7885fea4e606107831bd90441
Simon Long <simon@raspberrypi.com>
2021-10-14 13:01:57 +0100

    Add file to disable glamor on Pi 1 and Zero

--- usr/lib/systemd/scripts/gldriver_test.sh
+++ usr/lib/systemd/scripts/gldriver_test.sh
@@ -31,3 +31,18 @@ EOF
 	fi
 fi
 
+if grep -q "^Revision\s*:\s*00[0-9a-fA-F][0-9a-fA-F]$" /proc/cpuinfo || grep -q "^Revision\s*:\s*[ 123][0-9a-fA-F][0-9a-fA-F][0-9a-fA-F]0[0-369cC][0-9a-fA-F]$" /proc/cpuinfo ; then
+	if ! [ -e /usr/share/X11/xorg.conf.d/20-noglamor.conf ] ; then
+		cat > /usr/share/X11/xorg.conf.d/20-noglamor.conf << EOF
+Section "Device"
+	Identifier "kms"
+	Driver "modesetting"
+	Option "AccelMethod" "none"
+EndSection
+EOF
+	fi
+else
+	if [ -e /usr/share/X11/xorg.conf.d/20-noglamor.conf ] ; then
+		rm /usr/share/X11/xorg.conf.d/20-noglamor.conf
+	fi
+fi
