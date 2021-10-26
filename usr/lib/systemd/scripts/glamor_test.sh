9dcf8a73d54b15adaf6678074b5f044ccbbba2a5
Simon Long <simon@raspberrypi.com>
2021-10-26 10:04:57 +0100

    Modify glamor test to not disable glamor under legacy video driver

--- usr/lib/systemd/scripts/glamor_test.sh
+++ usr/lib/systemd/scripts/glamor_test.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
-if grep -q "^Revision\s*:\s*00[0-9a-fA-F][0-9a-fA-F]$" /proc/cpuinfo || grep -q "^Revision\s*:\s*[ 123][0-9a-fA-F][0-9a-fA-F][012][0-9a-fA-F][0-9a-fA-F][0-9a-fA-F]$" /proc/cpuinfo ; then
+# Glamor should run unless the platform is not Pi 4 and the legacy driver is not in use.
+# First line identifies platforms earlier than Pi 4; second line identifies whether KMS or fKMS are running.
+
+if { grep -s -q "^Revision\s*:\s*00[0-9a-fA-F][0-9a-fA-F]$" /proc/cpuinfo || grep -q "^Revision\s*:\s*[ 123][0-9a-fA-F][0-9a-fA-F][012][0-9a-fA-F][0-9a-fA-F][0-9a-fA-F]$" /proc/cpuinfo; } && \
+	{ grep -s -q okay /proc/device-tree/soc/v3d@7ec00000/status || grep -s -q okay /proc/device-tree/soc/firmwarekms@7e600000/status || grep -s -q okay /proc/device-tree/v3dbus/v3d@7ec04000/status; } ; then
 	if ! [ -e /usr/share/X11/xorg.conf.d/20-noglamor.conf ] ; then
 		cat > /usr/share/X11/xorg.conf.d/20-noglamor.conf << EOF
 Section "Device"
