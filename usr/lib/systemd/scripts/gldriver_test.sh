ebadc504ebbb2de3a3931244eeb1024c1d3d5f31
Simon Long <simon@raspberrypi.com>
2022-03-01 11:35:38 +0000

    Use raspi-config for hardware type and KMS tests

--- usr/lib/systemd/scripts/gldriver_test.sh
+++ usr/lib/systemd/scripts/gldriver_test.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 
-if grep -s -q okay /proc/device-tree/soc/v3d@7ec00000/status \
-                   /proc/device-tree/soc/firmwarekms@7e600000/status \
-                   /proc/device-tree/v3dbus/v3d@7ec04000/status ||
-   [ "$(arch)" == aarch64 ]; then
+if raspi-config nonint is_kms || [ "$(arch)" == aarch64 ]; then
 	if [ -e /usr/share/X11/xorg.conf.d/99-fbturbo.conf ] ; then
 		rm /usr/share/X11/xorg.conf.d/99-fbturbo.conf
 	fi
